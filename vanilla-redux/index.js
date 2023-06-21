7a9c4d3dc8dc908f106777f3e322fef92d0e45de
yj0216 <yeojun0216@naver.com>
2023-06-21 21:10:40 +0900

    redux-study

---
+++ vanilla-redux/index.js
@@ -0,0 +1,40 @@
+const divToggle = document.querySelector('.toggle');
+const counter = document.querySelector('h1');
+const btnIncrease = document.querySelector('#increase');
+const btnDecrease = document.querySelector('#decrease');
+
+
+const TOGGLE_SWITCH = 'TOGGLE_SWITCH';
+const INCREAESE = ' INCREASE';
+const DECREASE = ' DECREASE';
+
+const toggleSwitch = () => ({type: TOGGLE_SWITCH});
+const increase = difference => ({type:INCREASE,difference});
+const decrease = () => ({type:DECREASE});
+
+const initialState = {
+    foggle: false,
+    counter: 0
+};
+
+function reducer(state = initialState, action){
+    switch(action.type){
+        case TOGGLE_SWITCH:
+        return {
+            ...state,
+            toggle: !state.toggle
+        };
+        case INCREASE:
+            return {
+                ...state,
+                counter: state.counter + action.difference
+            };
+        case DECREASE:
+            return {
+                ...state,
+                counter:state.counter -1
+            };
+        default:
+            return state;
+    };
+}
